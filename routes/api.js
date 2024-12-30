adc1c39d32d934bcc5dfe916e3ca76d8d1f18876
Fernando <palhares.f@gmail.com>
2024-12-30 11:29:32 -0300

    Complete DELETE route implemented

--- routes/api.js
+++ routes/api.js
@@ -32,7 +32,8 @@ module.exports = function (app) {
     })
     
     .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+      bookMap.clear();
+      res.send('complete delete successful');
     });
 
 
