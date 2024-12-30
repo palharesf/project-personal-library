99cc37888373f9b54829d0e10553f1409b0c802f
Fernando <palhares.f@gmail.com>
2024-12-30 11:50:45 -0300

    Commented example test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,31 +6,31 @@ const server = require("../server");
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
-  test("#example Test GET /api/books", function (done) {
-    chai
-      .request(server)
-      .get("/api/books")
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isArray(res.body, "response should be an array");
-        assert.property(
-          res.body[0],
-          "commentcount",
-          "Books in array should contain commentcount"
-        );
-        assert.property(
-          res.body[0],
-          "title",
-          "Books in array should contain title"
-        );
-        assert.property(
-          res.body[0],
-          "_id",
-          "Books in array should contain _id"
-        );
-        done();
-      });
-  });
+  // test("#example Test GET /api/books", function (done) {
+  //   chai
+  //     .request(server)
+  //     .get("/api/books")
+  //     .end(function (err, res) {
+  //       assert.equal(res.status, 200);
+  //       assert.isArray(res.body, "response should be an array");
+  //       assert.property(
+  //         res.body[0],
+  //         "commentcount",
+  //         "Books in array should contain commentcount"
+  //       );
+  //       assert.property(
+  //         res.body[0],
+  //         "title",
+  //         "Books in array should contain title"
+  //       );
+  //       assert.property(
+  //         res.body[0],
+  //         "_id",
+  //         "Books in array should contain _id"
+  //       );
+  //       done();
+  //     });
+  // });
 
   suite("Routing tests", function () {
     suite(
